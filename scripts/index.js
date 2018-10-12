ab3ef78d8f6a06d294def9d912b730cffe8228d3
mkdir-helen <experiment.helen@gmail.com>
2018-10-12 14:03:54 -0400

    added image arrays

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
 //Fakestagram
+const IMAGES = [
+    "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=362a7baf8e58eab5f477b13befc0e62a&auto=format&fit=crop&w=1401&q=80",
+    "https://images.unsplash.com/photo-1488015795646-7e22a773d72a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=85f3d3ca03eab737cc448726428d19d0&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec224d4e2d53871f65b3a3ee1bae0638&auto=format&fit=crop&w=1355&q=80",
+    "https://images.unsplash.com/photo-1529257414772-1960b7bea4eb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=99a036a823f65958ab245848f5091dd7&auto=format&fit=crop&w=1350&q=80"
+];
